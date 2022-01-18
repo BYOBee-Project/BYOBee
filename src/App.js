0f8ad8ff1e61e059f17e8741115813ceaadfac87
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-01-18 10:29:02 -0800

    added routes to app component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './views/Home/Home';
+import BeeDetail from './components/BeeDetail/BeeDetail';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/:id" component={BeeDetail} />
         </Switch>
       </BrowserRouter>
     </div>

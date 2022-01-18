8a07f514013427bcdcb227a2da1e97a8f526f9af
zcaldwell <mintyst.john@gmail.com>
2022-01-18 11:02:59 -0800

    header added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './views/Home/Home';
 import BeeDetail from './components/BeeDetail/BeeDetail';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
+        <Header />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/:id" component={BeeDetail} />

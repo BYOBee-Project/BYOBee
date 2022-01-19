f122ddf4f6331670114e937d62edf0630db3ec4c
Emma Egstad <emma@egstad.com>
2022-01-19 11:42:35 -0800

    added loading states

--- src/views/UserDetail/UserDetail.js
+++ src/views/UserDetail/UserDetail.js
@@ -6,14 +6,17 @@ export default function UserDetail(props) {
   const id = props.match.params.id;
 
   const [userBee, setUserBee] = useState({});
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await findUserBee(id);
       setUserBee(data);
+      setLoading(false);
     };
     fetchData();
   }, [id]);
 
+  if (loading) return <h1 className="loader">Loading...</h1>;
   return <div>{userBee.name}</div>;
 }
